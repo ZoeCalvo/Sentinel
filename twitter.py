7d592581179df1b5c2fcdc0697f91d8a3fde3708
Zoe <zcs0001@alu.ubu.es>
2020-01-31 17:37:32 +0100

    Conexión con la API de Twitter

---
+++ twitter.py
@@ -0,0 +1,35 @@
+import tweepy
+import json
+
+#Autenticación
+access_token = "***REMOVED***"
+access_token_secret = "***REMOVED***"
+api_key = "***REMOVED***"
+api_secret_key = "***REMOVED***"
+
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
+auth.set_access_token(access_token, access_token_secret)
+
+#Objeto a través del cual vamos a realizar las llamadas a la API de Twitter
+#Los parámetros wait_on_rate_limit se utilizan para que si se cumple con el cupo de tweets cargados, la app no se pare
+#si no que espere un tiempo y cuando pueda continúe
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+#Obtener información sobre mi usuario
+# data = api.me()
+# print(json.dumps(data._json, indent=2))
+
+#Obtener información sobre otro usuario
+# data = api.get_user("nike")
+# print(json.dumps(data._json, indent=2))
+
+#Obtener el timeline
+# for tweet in tweepy.Cursor(api.user_timeline, screen_name="nike", tweet_mode="extended").items(1):
+#     print(json.dumps(tweet._json, indent=2))
+
+#Buscar tweets
+# for tweet in tweepy.Cursor(api.search, q="viernes", tweet_mode="extended").items(1):
+#     print(json.dumps(tweet._json, indent=2))
+for tweet in tweepy.Cursor(api.search, q="#Estudio3Sector2019", tweet_mode="extended").items(1):
+#     print(json.dumps(tweet._json, indent=2))
+    print(tweet._json["full_text"])

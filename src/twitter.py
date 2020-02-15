86cf7374b930b88ec90b625879d86bfdcf81f1f7
Zoe <zcs0001@alu.ubu.es>
2020-02-15 13:25:48 +0100

    Implementada búsqueda de tweets por fecha y creación de menú provisional

--- src/twitter.py
+++ src/twitter.py
@@ -29,8 +29,50 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 #     print(json.dumps(tweet._json, indent=2))
 
 #Buscar tweets
-# for tweet in tweepy.Cursor(api.search, q="viernes", tweet_mode="extended").items(1):
-#     print(json.dumps(tweet._json, indent=2))
-for tweet in tweepy.Cursor(api.search, q="#Estudio3Sector2019", tweet_mode="extended").items(1):
-#     print(json.dumps(tweet._json, indent=2))
-    print(tweet._json["full_text"])
+
+
+#     print(tweet._json["full_text"], tweet.text.encode("utf-8"))
+
+def searchHashtag(hashtag, since_date=None, until_date=None):
+    for tweet in tweepy.Cursor(api.search, q=hashtag, tweet_mode="extended", since=since_date, until=until_date).items():
+        print(tweet._json["full_text"])
+
+    return None
+
+def searchUser(user, since_date=None, until_date=None):
+    for tweet in tweepy.Cursor(api.search, q=user, tweet_mode="extended", since=since_date, until=until_date).items():
+        print(tweet._json["full_text"])
+
+    return None
+
+def searchWord(word, since_date=None, until_date=None):
+    for tweet in tweepy.Cursor(api.search, q=word, tweet_mode="extended", since=since_date, until=until_date).items():
+        print(tweet._json["full_text"])
+
+    return None
+
+
+
+if __name__ == "__main__":
+    print("Selecciona una opción")
+    print("1. Buscar tweets en un hashtag")
+    print("2. Buscar tweets donde aparezca un usuario mencionado")
+    print("3. Buscar tweets relacionados con una palabra")
+
+    option = input("Introduce el número de la opción deseada: ")
+
+    if option == "1":
+        hashtag = input("Introduzca el hashtag con #: ")
+        since_date = input("Introduce la fecha de comienzo de búsqueda: ")
+        until_date = input("Introduce la fecha de fin de búsqueda: ")
+        searchHashtag(hashtag, since_date, until_date)
+    if option == "2":
+        user = input("Introduzca el nombre del usuario con @: ")
+        since_date = input("Introduce la fecha de comienzo de búsqueda: ")
+        until_date = input("Introduce la fecha de fin de búsqueda: ")
+        searchUser(user, since_date, until_date)
+    if option == "3":
+        word = input("Introduce la palabra: ")
+        since_date = input("Introduce la fecha de comienzo de búsqueda: ")
+        until_date = input("Introduce la fecha de fin de búsqueda: ")
+        searchWord(word, since_date, until_date)

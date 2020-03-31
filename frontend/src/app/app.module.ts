f6d90f4118b7373bd22e8fff96072daa09206c16
Zoe <zcs0001@alu.ubu.es>
2020-03-31 19:34:06 +0200

    Integrado template de gráficos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
+import { ChartsModule } from 'ng2-charts';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { ExamplesModule } from './examples/examples.module';
-import {HttpBackend, HttpClientModule} from '@angular/common/http';
+import { HttpBackend, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -21,6 +23,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
         NgbModule.forRoot(),
         FormsModule,
         RouterModule,
+        ChartsModule,
+        ToastrModule.forRoot(),
         AppRoutingModule,
         ComponentsModule,
         ExamplesModule,

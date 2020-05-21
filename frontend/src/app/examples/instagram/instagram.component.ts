f0aa3ae8a0b925e1631975e20e521c367ce9410e
Zoe <zcs0001@alu.ubu.es>
2020-05-21 11:48:20 +0200

    Implementado datepicker en la pantalla de búsqueda de ig

--- frontend/src/app/examples/instagram/instagram.component.ts
+++ frontend/src/app/examples/instagram/instagram.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-instagram',
@@ -9,6 +10,8 @@ export class InstagramComponent implements OnInit {
   focus;
   focus1;
   is_tw;
+  model: NgbDateStruct;
+  model1: NgbDateStruct;
   constructor() { }
 
   ngOnInit() {

5702e403276bf389c488d22a34af9eaa813abee6
Zoe <zcs0001@alu.ubu.es>
2020-06-09 23:35:56 +0200

    Arreglado que desapareciese la celda de password cuando era incorrecta

--- frontend/src/app/examples/login/login.component.ts
+++ frontend/src/app/examples/login/login.component.ts
@@ -60,7 +60,7 @@ export class LoginComponent implements OnInit {
           });
 
         } else {
-          document.getElementById('passwd').remove();
+          
           this.alerts.push({
             id: 2,
             type: 'warning',
